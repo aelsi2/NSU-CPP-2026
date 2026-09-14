9a658b88f75cb291ab26e0f5950e8562f4d97cfe
aelsi2 <eliseevandrey2005@gmail.com>
2026-09-15 02:56:11 +0700

    Ignore .conan/profile and build.sh

--- task1/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-project_dir="$(dirname -- "$0")"
-build_dir="$project_dir/build"
-
-conan install . -pr="$project_dir/.conan/profile" --output-folder "$build_dir" --build=missing
-cmake --preset conan-release
-cmake --build --preset conan-release
